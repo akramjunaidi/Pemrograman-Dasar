35435aadbbcb2a2e728aae23fff5ac02f6e11ca1
Akram Junaidi <250306033.mhs@uinmataram.ac.id>
2025-10-03 14:15:25 +0800

    PPD3, latihan 2
    Pertemuan Pemrograman Dasar ke-3, latihan 2

---
+++ PD3, example.py
@@ -0,0 +1,11 @@
+#PD3, example
+var_c = int(input("masukkan nilai var_c: "))
+var_d = int(input("masukkan nilai var_d: "))
+a = var_c+var_d
+b = var_c-var_d
+c = var_c*var_d
+d = var_c/var_d
+print(a)
+print(b)
+print(c)
+print(d)
