e71265a74f38058112844e055352f934a3c06177
Akram Junaidi <250306033.mhs@uinmataram.ac.id>
2025-10-03 14:18:00 +0800

    PPD3, latihan 2
    Pertemuan Pemrograman Dasar, latihan 2

---
+++ #PD3, example 2.py
@@ -0,0 +1,21 @@
+#PD3, example 2
+var_e = int(input("masukkan nilai var_e: "))
+var_f = int(input("masukkan nilai var_f: "))
+var_g = int(input("masukkan nilai var_g: "))
+var_h = int(input("masukkan nilai var_h: "))
+a = var_e+var_f
+b = var_e-var_f
+c = var_e*var_f
+d = var_e/var_f
+e = var_g+var_h
+f = var_g-var_h
+g = var_g*var_h
+h = var_g/var_h
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
