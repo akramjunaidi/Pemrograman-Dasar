e378bd250497b6bf2e52755a08d3990e03b3b808
Akram Junaidi <250306033.mhs@uinmataram.ac.id>
2025-10-11 16:54:21 +0800

    Add files via upload

---
+++ PPD4, example var dinamis 3.py
@@ -0,0 +1,7 @@
+UAS = int(input("masukkan nilai:"))
+if(UAS >= 70):
+    print("Nilai Anda Bagus!")
+elif(UAS >= 51 and UAS < 70):
+    print("Nilai Anda Kurang!")
+else:
+    print("Anda Tidak Lulus!")
