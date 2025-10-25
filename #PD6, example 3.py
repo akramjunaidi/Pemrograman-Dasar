40132c6da1d02a6b3e8e0d7a3308a1802ec67113
Akram Junaidi <250306033.mhs@uinmataram.ac.id>
2025-10-25 11:16:51 +0800

    PDD6, example 3

---
+++ #PD6, example 3.py
@@ -0,0 +1,14 @@
+#PD6, example 4
+
+#list mula-mula
+buah = ["jeruk", "apel", "mangga", "durian"]
+
+#inputan dari keyboard
+#menambahkan nama buah baru
+new_buah = input("Masukkan nama buah baru: ")
+buah.append(new_buah)
+
+#tampilkan 
+for nama_buah in buah:
+    print(nama_buah)
+
