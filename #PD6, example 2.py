3a8c5e61fd65ca6ed4f268dc617e8195a5380faa
Akram Junaidi <250306033.mhs@uinmataram.ac.id>
2025-10-25 11:16:29 +0800

    PDD6, example 2

---
+++ #PD6, example 2.py
@@ -0,0 +1,11 @@
+#PD6, example 3
+
+#list mula-mula
+buah = ["jeruk", "apel", "mangga", "durian"]
+
+#tambahkan manggis
+buah.append("manggis")
+
+#ta,bahkan manggis
+for nama_buah in buah:
+    print(nama_buah)
